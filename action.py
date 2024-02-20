aebbb0ef9183e0189c06a7a0652f6db482c6e159
hexdcode <160484496+hexdcode@users.noreply.github.com>
2024-02-20 14:09:29 +0800

    Update action.py

--- action.py
+++ action.py
@@ -10,7 +10,7 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-driver.get("https://python.org")
+driver.get("https://rili.jin10.com/day/2024-02-23")
 
 with open('test.html', 'w', encoding='utf-8') as f:
     f.write(driver.page_source)
